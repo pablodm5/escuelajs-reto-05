28f9fb82015f3419ecf9cef0587bcf0e1e449e39
saforus <1405pablo@gmail.com>
2019-09-28 18:15:07 -0500

    RETO 5, resueltos los 4 problemas

--- src/index.js
+++ src/index.js
@@ -1,38 +1,55 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
-
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+const API = "http://us-central1-escuelajs-api.cloudfunctions.net/characters";
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
+      localStorage.setItem("next_fetch", `${response.info.next}`);
+      console.log(response.info.next);
       const characters = response.results;
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
+      let output;
+      if (response.info.next) {
+        output = characters
+          .map(character => {
+            return `
+          <article class="Card">
+          <img src="${character.image}" />
+          <h2>${character.name}<span>${character.gender}</span></h2>
+          </article>
+          `;
+          })
+          .join("");
+      } else {
+        intersectionObserver.disconnect();
+        output = `<div class="endResults">Ya no hay mas personajes :(</div>`;
+      }
+      let newItem = document.createElement("section");
+      newItem.classList.add("Items");
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
-}
+};
 
-const loadData = () => {
-  getData(API);
-}
-
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
+async function loadData() {
+  if (localStorage.getItem("next_fetch") == null) {
+    await getData(API);
+  } else {
+    await getData(localStorage.getItem("next_fetch"));
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+}
 
+const intersectionObserver = new IntersectionObserver(
+  entries => {
+    if (entries[0].isIntersecting) {
+      loadData();
+    }
+  },
+  {
+    rootMargin: "0px 0px 100% 0px"
+  }
+);
+
+localStorage.clear();
 intersectionObserver.observe($observe);
